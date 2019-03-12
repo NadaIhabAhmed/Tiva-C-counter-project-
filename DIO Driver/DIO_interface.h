6b5ea3c39f89d4d735859d77ed322db122a16654
NadaIhabAhmed <36302376+NadaIhabAhmed@users.noreply.github.com>
2019-03-12 20:14:17 +0200

    Add files via upload

---
+++ DIO Driver/DIO_interface.h
@@ -0,0 +1,40 @@
+#ifndef DIO_INTERFACE_H_
+#define DIO_INTERFACE_H_
+#include <stdint.h>
+
+#define OUTPUT		1
+#define INPUT		0
+
+#define	HIGH		1
+#define LOW			0
+
+#define PORTA 0
+#define PORTB 1
+#define PORTC 2
+#define PORTD 3
+#define PORTE 4
+#define PORTF 5
+
+enum Dio_LevelType{
+			STD_LOW, // 0
+			STD_HIGH // 1
+};
+
+/*DIO_ReadPort: 
+This function returns the value of the pins selected by pins_masks in the port selected by port_index 
+(GET_BITS())
+*/
+uint8_t DIO_ReadPort (uint8_t port_index, uint8_t pin_mask); 
+/*DIO_WritePort: 
+This function Change the value of the pins selected by pins_masks in the port selected by port_index to input pins_level
+(SET_BITS())
+*/
+void DIO_WrirePort (uint8_t port_index, uint8_t pin_mask, uint8_t pin_level);
+/*DIO_FlipPort: 
+This function Toggle the values of the pins selected by pins_masks in the port selected by port_index
+(TOGGLE_BITS())
+*/
+void DIO_FlipPort (uint8_t port_index, uint8_t pin_mask);
+
+
+#endif /* DIO_INTERFACE_H_ */
---
+++ DIO Driver/DIO_prog.c
@@ -0,0 +1,146 @@
+#include "tm4c123gh6pm.h"
+#include "DIO_interface.h"
+
+
+uint8_t DIO_ReadPort (uint8_t port_index, uint8_t pin_mask)
+{
+	switch(port_index)
+	{
+		case 0: //PORTA
+			return GPIO_PORTA_DATA_R & pin_mask;
+		
+		case 1://PORTB
+			return GPIO_PORTB_DATA_R & pin_mask;
+	
+		case 2://PORTC
+			return GPIO_PORTC_DATA_R & pin_mask;
+			
+		case 3://PORTD
+			return GPIO_PORTD_DATA_R & pin_mask;
+			
+		case 4://PORTE
+			return GPIO_PORTE_DATA_R & pin_mask;
+		
+		case 5://PORTF
+			return GPIO_PORTF_DATA_R & pin_mask;
+
+	}
+}
+
+void DIO_WrirePort (uint8_t port_index, uint8_t pin_mask, uint8_t pins_level)
+{
+	switch(pins_level)
+	{
+		case 0: // make them low----------------CLEAR BITS
+			if (port_index == 0){
+				GPIO_PORTA_DATA_R &= ~(pin_mask);
+			}
+			else if(port_index == 1){
+				GPIO_PORTB_DATA_R &= ~(pin_mask);
+			}
+			else if(port_index == 2){
+				GPIO_PORTC_DATA_R &= ~(pin_mask);
+			}
+			else if(port_index == 3){
+				GPIO_PORTD_DATA_R &= ~(pin_mask);
+			}
+			else if(port_index == 4){
+				GPIO_PORTE_DATA_R &= ~(pin_mask);
+			}
+			else if(port_index == 5){
+				GPIO_PORTF_DATA_R &= ~(pin_mask);
+			}
+			
+		/*	switch(port_index)
+			{
+				case 0: //PORTA
+					GPIO_PORTA_DATA_R &= ~(pin_mask);
+				
+				case 1://PORTB
+					GPIO_PORTB_DATA_R &= ~(pin_mask);
+			
+				case 2://PORTC
+					GPIO_PORTC_DATA_R &= ~(pin_mask);
+					
+				case 3://PORTD
+					GPIO_PORTD_DATA_R &= ~(pin_mask);
+					
+				case 4://PORTE
+					GPIO_PORTE_DATA_R &= ~(pin_mask);
+				
+				case 5://PORTF
+					GPIO_PORTF_DATA_R &= ~(pin_mask);
+		
+			}	*/
+			break;
+		case 1: // make them high--------------SET BITS
+			if (port_index == 0){
+				GPIO_PORTA_DATA_R |= pin_mask;
+			}
+			else if(port_index == 1){
+				GPIO_PORTB_DATA_R |= pin_mask;
+			}
+			else if(port_index == 2){
+				GPIO_PORTC_DATA_R |= pin_mask;
+			}
+			else if(port_index == 3){
+				GPIO_PORTD_DATA_R |= pin_mask;
+			}
+			else if(port_index == 4){
+				GPIO_PORTE_DATA_R |= pin_mask;
+			}
+			else if(port_index == 5){
+				GPIO_PORTF_DATA_R |= pin_mask;
+			}
+			/*
+			switch(port_index)
+			{
+				case 0: //PORTA
+					GPIO_PORTA_DATA_R |= pin_mask;
+					break;
+				case 1://PORTB
+					GPIO_PORTB_DATA_R |= pin_mask;
+					break;
+				case 2://PORTC
+					GPIO_PORTC_DATA_R |= pin_mask;
+					break;
+				case 3://PORTD
+					GPIO_PORTD_DATA_R |= pin_mask;
+					break;
+				case 4://PORTE
+					GPIO_PORTE_DATA_R |= pin_mask;
+					break;
+				case 5://PORTF
+					GPIO_PORTF_DATA_R |= pin_mask;
+					break;
+			}	*/
+			break;
+	}
+	
+}
+
+void DIO_FlipPort (uint8_t port_index, uint8_t pin_mask)
+{
+	switch(port_index)//----------TOGGLE BITS
+	{
+		case 0: //PORTA
+			GPIO_PORTA_DATA_R ^= pin_mask;
+			break;
+		case 1://PORTB
+			GPIO_PORTB_DATA_R ^= pin_mask;
+			break;
+		case 2://PORTC
+			GPIO_PORTC_DATA_R ^= pin_mask;
+			break;
+		case 3://PORTD
+			GPIO_PORTD_DATA_R ^= pin_mask;
+			break;
+		case 4://PORTE
+			GPIO_PORTE_DATA_R ^= pin_mask;
+			break;
+		case 5://PORTF
+			GPIO_PORTF_DATA_R ^= pin_mask;
+			break;
+	}
+}
+

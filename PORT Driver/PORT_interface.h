07abcf6086de477365784d015d7010d145545f41
NadaIhabAhmed <36302376+NadaIhabAhmed@users.noreply.github.com>
2019-03-20 10:56:09 +0200

    Update PORT_interface.h

--- PORT Driver/PORT_interface.h
+++ PORT Driver/PORT_interface.h
@@ -5,7 +5,7 @@
 enum Port_PinDirectionType{
 			PORT_PIN_IN,
 			PORT_PIN_OUT
-};
+}dir;
 /*Port_Init:
 Initialize port based on selected port_index (0 to 5) by enabling the clock, unlocking the port, and making the selected mode digital
 */
@@ -14,7 +14,7 @@ void Port_Init(uint8_t port_index);
 /* Port_SetPinDirection:
 Change the direction of the selected pins by pins_mask in the port selected by port_index
 */
-void Port_SetPinDirection(uint8_t port_index, uint8_t pins_mask, uint8_t pins_direction);
+void Port_SetPinDirection(uint8_t port_index, uint8_t pins_mask, dir pins_direction);
 /*Port_SetPinPullUp:
 If enable is 1, the selected pins by pins_mask in the port selected by port_index will beconnected to internal pull-up resistor.
 If enable is 0, the selected pins by pins_mask in the port selected by port_index will be not be connected to internal pull-up resistor.
